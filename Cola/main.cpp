f4a27257f1f4f1c69f86e3fb4b4f6c68ecd14cfe
Kali <edgaplljro4@gmail.com>
2025-08-27 16:01:08 -0300

    Cambios en el main de cola desde linux

--- Cola/main.cpp
+++ Cola/main.cpp
@@ -3,5 +3,9 @@
 #include "cola.h"
 
 int main(){
-    
+    Cola nueva;
+    crearCola(nueva);
+    insertarFinal(10, nueva);
+    int num = primeroEnCola(nueva);
+    std::cout << "El primer elemento es" << num<< std::endl;
 }
