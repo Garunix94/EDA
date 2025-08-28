291466d924c12c0c89fbcebc959400819cadd952
Garunix94 <edgaplljro4@gmail.com>
2025-08-28 12:47:08 -0300

    Algunas cosas nuevas al main

--- Cola/main.cpp
+++ Cola/main.cpp
@@ -3,5 +3,9 @@
 #include "cola.h"
 
 int main(){
+    Cola Cola;
+    crearCola(Cola);
+    insertarFinal(10, Cola);
+    std::cout << primeroEnCola(Cola) << " es el primero en la cola" <<std::endl;
     
 }
