be6f4458fff494e7a646151f365d4f84d6e5b306
Garunix94 <edgaplljro4@gmail.com>
2025-08-28 12:57:44 -0300

    Un minimo codigo en el main

--- Cola/main.cpp
+++ Cola/main.cpp
@@ -3,17 +3,10 @@
 #include "cola.h"
 
 int main(){
-<<<<<<< HEAD
-    Cola Cola;
-    crearCola(Cola);
-    insertarFinal(10, Cola);
-    std::cout << primeroEnCola(Cola) << " es el primero en la cola" <<std::endl;
-    
-=======
+
     Cola nueva;
     crearCola(nueva);
     insertarFinal(10, nueva);
     int num = primeroEnCola(nueva);
-    std::cout << "El primer elemento es" << num<< std::endl;
->>>>>>> f4a27257f1f4f1c69f86e3fb4b4f6c68ecd14cfe
+    std::cout << "El primer elemento es: " << num<< std::endl;
 }
